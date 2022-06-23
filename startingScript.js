f6a329a92b0c48671fb09673300226a37b580fe0
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-06-23 21:20:19 +0300

    Renamed html files, reworked the mousemove event on main html. changed navigation, added global css file

---
+++ startingScript.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(document).mousemove(function(e) {
+      // var images = ['url("images/MainImages/MainImg0.png")', 'url("images/MainImages/MainImg1.png")','url("images/MainImages/MainImg2.png")', 'url("images/MainImages/MainImg3.png")', 'url("images/MainImages/MainImg4.png")', 'url("images/MainImages/MainImg5.jpg")', 'url("images/MainImages/MainImg6.png")', 'url("images/MainImages/MainImg7.png")', 'url("images/MainImages/MainImg8.png")', 'url("images/MainImages/MainImg9.png")', 'url("images/MainImages/MainImg10.jpg")',];
+      // $(".box").css('background-image', images[Math.floor(Math.random() * images.length)]);
+    });  
+  setInterval(function() {
+    var currentMousePosition = {x: - 1, y: -1}
+    $(document).mousemove(function(e) {
+      currentMousePosition.x = e.pageX;
+      currentMousePosition.y = e.pageY;
+      console.log(currentMousePosition.x);
+      if (currentMousePosition.x % 8 == 0 || currentMousePosition.y % 8 == 0)
+      { 
+        console.log('lalka')
+      var images = ['url("images/MainImages/MainImg0.png")', 'url("images/MainImages/MainImg1.png")','url("images/MainImages/MainImg2.png")', 'url("images/MainImages/MainImg3.png")', 'url("images/MainImages/MainImg4.png")', 'url("images/MainImages/MainImg5.jpg")', 'url("images/MainImages/MainImg6.png")', 'url("images/MainImages/MainImg7.png")', 'url("images/MainImages/MainImg8.png")', 'url("images/MainImages/MainImg9.png")', 'url("images/MainImages/MainImg10.jpg")',];
+      $(".box").css('background-image', images[Math.floor(Math.random() * images.length)]);
+    }
+    });
+  },1000);
+  // смена картинок по движению мыши 
+  let constrain = 20;
+  let mouseOverContainer = document.getElementById("lines");
+  let ex1Layer = document.getElementById("ex1-layer");
+
+  function transforms(x, y, el) {
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - (box.height / 2)) / constrain;
+    let calcY = (x - box.x - (box.width / 2)) / constrain;
+    return "perspective(4000px) "
+      + "   rotateX("+ calcX +"deg) "
+      + "   rotateY("+ calcY +"deg) ";
+  };
+   function transformElement(el, xyEl) {
+    el.style.transform  = transforms.apply(null, xyEl);
+  }
+  $(document).mousemove(function(e){
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([ex1Layer]);
+    console.log(position);
+    window.requestAnimationFrame(function(){
+      transformElement(ex1Layer, position);
+    });
+  });
+
+});
--- test.js
+++ test.js
@@ -1,36 +1,39 @@
-// var fadeStart=1300; // 100px scroll or less will equiv to 1 opacity
-// var fadeUntil=1400; // 200px scroll or more will equiv to 0 opacity
-// var fading = $('#lol');
-//
-// $(window).bind('scroll', function(){
-//     var offset = $(document).scrollTop();
-//     var opacity=0;
-//     ;
-//     if( offset<=fadeStart ){
-//         opacity=1;
-//     }else if( offset<=fadeUntil ){
-//         opacity=1-offset/fadeUntil;
+var fadeStart=1300; // 100px scroll or less will equiv to 1 opacity
+var fadeUntil=1400; // 200px scroll or more will equiv to 0 opacity
+var fading = $('#lol');
+
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop();
+    var opacity=0;
+    console.log(offset);
+    ;
+    if( offset<=fadeStart ){
+        opacity=1;
+    }else if( offset<=fadeUntil ){
+        opacity=1-offset/fadeUntil;
+    }
+    $('#lol').css('opacity',opacity);
+});
+
+
+// $(document).ready(function(){
+//   var checktheheight = setInterval(function(){
+//     let exItem = $('.wow')[0].getBoundingClientRect();
+//     console.log(exItem);
+//     if (exItem.y <= -300){
+//       $('.wow').addClass("transition");
+//     } else if(exItem.y > -100) {
+//       $('.wow').removeClass("transition");
 //     }
-//     $('#lol').css('opacity',opacity);
-// });
-$(document).ready(function(){
-  var checktheheight = setInterval(function(){
-    let exItem = $('.wow')[0].getBoundingClientRect();
-    console.log(exItem);
-    if (exItem.y <= -300){
-      $('.wow').addClass("transition");
-    } else if(exItem.y > -100) {
-      $('.wow').removeClass("transition");
-    }
-    if (exItem.y <= - 1500) {
-      $('.wow1').addClass("transition");
-    } else if(exItem.y > -1500) {
-      $('.wow1').removeClass("transition");
-    }
-    if (exItem.y <= -3000) {
-      $('.wow2').addClass("transition");
-    } else if (exItem.y > -3000) {
-      $('.wow2').removeClass('transition');
-    }
-  }, 1000)
-})
+//     if (exItem.y <= - 1500) {
+//       $('.wow1').addClass("transition");
+//     } else if(exItem.y > -1500) {
+//       $('.wow1').removeClass("transition");
+//     }
+//     if (exItem.y <= -3000) {
+//       $('.wow2').addClass("transition");
+//     } else if (exItem.y > -3000) {
+//       $('.wow2').removeClass('transition');
+//     }
+//   }, 1000)
+// })
